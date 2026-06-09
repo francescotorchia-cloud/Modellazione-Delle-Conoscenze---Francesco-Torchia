8eb8082448b5be5712fa5f9c84895bf992e47cab
Francesco Torchia <francesco.torchia@studenti.unicam.it>
2026-06-09 15:30:18 +0200

    Chore: setup iniziale progetto con Gradle e wrapper

---
+++ MpgcRpg/src/main/java/it/unicam/cs/mpgc/rpg125939/mpgcrpg/Launcher.java
@@ -0,0 +1,9 @@
+package it.unicam.cs.mpgc.rpg125939.mpgcrpg;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ MpgcRpg/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module it.unicam.cs.mpgc.rpg125939.mpgcrpg {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens it.unicam.cs.mpgc.rpg125939.mpgcrpg to javafx.fxml;
+    exports it.unicam.cs.mpgc.rpg125939.mpgcrpg;
+}
